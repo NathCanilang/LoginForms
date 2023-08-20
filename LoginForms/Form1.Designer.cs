a55c6811e4b468c159d81509b26462003db5239f
Starlapot@xvk.me <Starlapot@xvk.me>
2023-08-20 22:41:13 +0800

    Hi nath :)

--- LoginForms/Form1.Designer.cs
+++ LoginForms/Form1.Designer.cs
@@ -29,33 +29,56 @@
         private void InitializeComponent()
         {
             this.BackBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
             // 
             // BackBtn
             // 
-            this.BackBtn.Location = new System.Drawing.Point(364, 348);
+            this.BackBtn.Location = new System.Drawing.Point(273, 283);
+            this.BackBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BackBtn.Name = "BackBtn";
-            this.BackBtn.Size = new System.Drawing.Size(75, 23);
+            this.BackBtn.Size = new System.Drawing.Size(56, 19);
             this.BackBtn.TabIndex = 0;
             this.BackBtn.Text = "Back";
             this.BackBtn.UseVisualStyleBackColor = true;
             this.BackBtn.Click += new System.EventHandler(this.BackBtn_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(327, 103);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(357, 129);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(49, 20);
+            this.maskedTextBox1.TabIndex = 2;
+            // 
             // miscellaneous
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.BackBtn);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "miscellaneous";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.miscellaneous_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button BackBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
     }
 }
